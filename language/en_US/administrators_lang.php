9241461b12722fed0e23bf260767a347314934f9
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-14 17:57:30 +0000

    - Updated translations

--- language/en_US/administrators_lang.php
+++ language/en_US/administrators_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['administrators_app_name'] = 'Administrators';
 $lang['administrators_app_description'] = 'With the Administrators app, you can grant access to specific apps to groups of users on the system.';
+$lang['administrators_app_name'] = 'Administrators';
