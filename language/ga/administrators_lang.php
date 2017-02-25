caf4de77565d3c17f93134fb294dab275e741f17
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:35:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Leis an app Riarthóirí, is féidir leat a thabhairt rochtain ar apps go sonrach le grúpaí úsáideoirí ar an gcóras.';
+$lang['administrators_app_name'] = 'riarthóirí';
