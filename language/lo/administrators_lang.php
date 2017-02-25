dd1e1fc6f7b681cde712319ec9a3be4290b6e9c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:36:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'ມີ app ຜູ້ບໍລິຫານ, ທ່ານສາມາດໃຫ້ການເຂົ້າເຖິງກິດສະເພາະເພື່ອກຸ່ມຂອງຜູ້ຊົມໃຊ້ໃນລະບົບ.';
+$lang['administrators_app_name'] = 'ຜູ້ບໍລິຫານ';
