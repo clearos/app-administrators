7415e11430ace4937013d91c14e503bbc40bb5a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:39:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'මෙම පරිපාලකයින් යෙදුම සමඟ, ඔබ පද්ධතිය භාවිතා කරන්නන් කණ්ඩායම් විශේෂිත යෙදුම් වෙත පිවිසුම් ලබා ගත හැක.';
+$lang['administrators_app_name'] = 'පරිපාලකවරුනි';
