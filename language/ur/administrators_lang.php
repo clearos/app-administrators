521a798fbee6b30f4955846361cf12385e5180dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:42:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'منتظمین اپلی کیشن کے ساتھ، آپ کے سسٹم پر صارفین کے گروپوں کے لئے مخصوص اطلاقات تک رسائی فراہم کر سکتے ہیں.';
+$lang['administrators_app_name'] = 'منتظمین';
