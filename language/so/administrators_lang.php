96cd0c905be1f8dcef7a5c303cefeee624648353
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:40:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Iyadoo app Maamulayaasha ah, waxaad siin kartaa helitaanka apps gaar ah si ay kooxaha dadka isticmaala on nidaamka.';
+$lang['administrators_app_name'] = 'Maamulayaasha';
