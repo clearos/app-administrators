d538facc29b2a322a7b8195b95e51c2801ff1f58
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:33:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Kun la administrantoj app, vi povas doni aliro al specifa apps al grupoj de uzantoj en la sistemo.';
+$lang['administrators_app_name'] = 'administrantoj';
