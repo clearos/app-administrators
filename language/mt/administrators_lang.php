4e99903f0d9e51437ca44d4b27134159108eebf0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:38:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Bl-app Amministraturi, inti tista &#39;tagħti aċċess għal apps speċifiċi għal gruppi ta&#39; utenti fuq is-sistema.';
+$lang['administrators_app_name'] = 'amministraturi';
