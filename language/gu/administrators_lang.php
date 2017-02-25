584ad2141dec099e0a30c66a9d39b14275ec1736
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:34:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'સંચાલક એપ્લિકેશન સાથે, તમે સિસ્ટમ પર વપરાશકર્તાઓ જૂથો માટે ખાસ એપ્લિકેશન્સ ઍક્સેસ આપી શકો છો.';
+$lang['administrators_app_name'] = 'સંચાલક';
