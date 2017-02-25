8963bca9b1a1c8b6e96a6bcbe7c845c1db5ec0d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:35:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Með Stjórnandi app, getur þú veita aðgang að tilteknum forritum til hópa notenda á kerfið.';
+$lang['administrators_app_name'] = 'stjórnendur';
