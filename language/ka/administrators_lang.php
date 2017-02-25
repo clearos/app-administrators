563a47322b04f42e593792d76d5100660bbd888b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:35:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'ერთად Administrators app, შეგიძლიათ დაშვების კონკრეტული apps ჯგუფების წევრებს სისტემა.';
+$lang['administrators_app_name'] = 'Administrators';
