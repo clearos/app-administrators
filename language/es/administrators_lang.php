17662577b83ef91543e144a05ba354215e14ac63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:40:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Con la aplicación Administradores, puede permitir el acceso a aplicaciones específicas para grupos de usuarios en el sistema.';
+$lang['administrators_app_name'] = 'administradores';
