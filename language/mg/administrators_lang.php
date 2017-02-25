639aff8c2dcf62a7f5e9984740517eac26201f08
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:37:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Ny Administrators fampiharana, dia afaka manome fidirana amin&#39;ny fampiharana ny vondrona manokana ny mpampiasa ao amin&#39;ny rafitra.';
+$lang['administrators_app_name'] = 'mpitantana';
