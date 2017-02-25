19beca9c2a8e7c8760d8349358340ff00a629311
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:42:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Gyda&#39;r app Gweinyddwyr, gallwch roi mynediad i apps penodol i grwpiau o ddefnyddwyr ar y system.';
+$lang['administrators_app_name'] = 'gweinyddwyr';
