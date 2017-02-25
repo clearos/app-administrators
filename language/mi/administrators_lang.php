34b8c58c10abd11c6c2fc0805dcb0e28fd8c8688
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:37:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Ki te taupānga Kaiwhakahaere, ka taea e koe tukua mai te uru ki taupānga motuhake ki ngā rōpū o ngā kaiwhakamahi i runga i te pūnaha.';
+$lang['administrators_app_name'] = 'kaiwhakahaere';
