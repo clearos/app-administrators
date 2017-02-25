aa5387ab2b1f94935d5aa70d69419c256d74d22d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:34:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Tare da ma&#39;aikata app, za ka iya baiwa damar yin amfani da takamaiman apps to kungiyoyin masu amfani a kan tsarin.';
+$lang['administrators_app_name'] = 'ma&#39;aikata';
