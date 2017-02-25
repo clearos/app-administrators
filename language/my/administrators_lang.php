b8546ed20fa0eb5e1d63a635a655f22e7a39cc43
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:32:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'ထိုအက်ဒမင်များ app ကိုအတူ, သင် system ပေါ်တွင်အသုံးပြုသူအုပ်စုများမှတိကျတဲ့ apps များမှဝင်ရောက်ခွင့်ပေးသနားတော်မူနိုင်ပါတယ်။';
+$lang['administrators_app_name'] = 'အုပ်ချုပ်ရေးမှူးများ';
