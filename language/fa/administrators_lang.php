73a3d6741526164ae87a6045726eb03439c936b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:38:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'با نرم افزار مدیران، شما می توانید دسترسی به برنامه های خاص به گروه از کاربران بر روی سیستم عطا کند.';
+$lang['administrators_app_name'] = 'مدیران';
