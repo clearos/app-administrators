081e081c7af90cc999fcd1e35bc94a34d391fa11
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:39:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Cu aplicația Administratorii, puteți acorda acces la anumite aplicații pentru grupuri de utilizatori pe sistem.';
+$lang['administrators_app_name'] = 'administratorii';
