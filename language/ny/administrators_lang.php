9282952ccbfae95eda56a7302a3d85c0ac53b8a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:32:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Ndi app oyang&#39;anira, mukhoza perekani mwayi kwa mapulogalamu mwachindunji kwa magulu a owerenga pa dongosolo.';
+$lang['administrators_app_name'] = 'olamulira';
