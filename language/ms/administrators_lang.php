7b6015e86d4de5455928e66ceef49d7e103ed481
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:38:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Dengan aplikasi Pentadbir, anda boleh memberikan akses kepada aplikasi khusus untuk kumpulan pengguna pada sistem.';
+$lang['administrators_app_name'] = 'pentadbir';
