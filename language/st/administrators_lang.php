85cbb9998613b2d4acc452c2f02568233524e285
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:40:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Le app e sa batsamaisi, u ka mo neha phihlello ho ditiriso tse tobileng tseo ho lihlopha tsa basebedisi tsamaiso.';
+$lang['administrators_app_name'] = 'batsamaisi';
