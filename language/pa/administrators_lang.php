786bc12f5ea2fb423d798740382cddf37a06b49b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:38:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'ਪਰਬੰਧਕ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਨਾਲ, ਤੁਹਾਨੂੰ ਸਿਸਟਮ ਤੇ ਉਪਭੋਗੀ ਦੇ ਗਰੁੱਪ ਨੂੰ ਖਾਸ ਐਪਸ ਨੂੰ ਪਹੁੰਚ ਦੇਣ ਹੋ ਸਕਦਾ ਹੈ.';
+$lang['administrators_app_name'] = 'ਪਰਬੰਧਕ';
