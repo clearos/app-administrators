bc3c4d86f826831e19b60c891dd1d6550b418e87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:42:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Administratorlar ilovasi bilan, siz tizimiga foydalanuvchilarning guruhlarga xos ilovalarga ruxsat mumkin.';
+$lang['administrators_app_name'] = 'Administratorlar';
