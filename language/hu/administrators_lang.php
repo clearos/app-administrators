2ede7c4f7e3af61cd9d8e1896bbd525b15909750
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:35:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'A rendszergazdák app, akkor megadja a hozzáférést bizonyos alkalmazásokat a felhasználói csoportok a rendszerben.';
+$lang['administrators_app_name'] = 'A rendszergazdák';
