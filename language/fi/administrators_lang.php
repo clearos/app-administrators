b467ba94af0aa62611df5bf2f643f70595fcf5ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:33:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Kun Ylläpitäjät app, voit myöntää käyttöoikeuden tiettyjen sovellusten käyttäjäryhmille järjestelmän.';
+$lang['administrators_app_name'] = 'Ylläpitäjät';
