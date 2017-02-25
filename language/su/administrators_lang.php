0ee14e317b4bff866e1f6bcc92196bf47410aa17
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:40:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Jeung aplikasi Kuncén, Anjeun bisa dibere aksés ka aktip husus pikeun grup pamaké dina sistem.';
+$lang['administrators_app_name'] = 'kuncén';
