2f119f72b0ed95f8d485fbb52bf5e7446c821c54
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:31:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Me app Administratorët, ju mund të japë qasje në aplikacione të veçanta për grupet e përdoruesve në sistemin.';
+$lang['administrators_app_name'] = 'administratorët';
