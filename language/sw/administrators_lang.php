a12509019829f642458e79b85a26c4cc926d2bac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:40:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Pamoja na programu Watawala, unaweza kutoa ruhusa kwa programu maalum kwa makundi ya watumiaji juu ya mfumo.';
+$lang['administrators_app_name'] = 'watawala';
