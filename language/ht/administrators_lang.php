814e4872f53c7346698471af724a164b9903f859
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:34:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Avèk app a Administratè, ou ka bay aksè a apps espesifik nan gwoup nan itilizatè sou sistèm nan.';
+$lang['administrators_app_name'] = 'administratè';
