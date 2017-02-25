1ab693837548483413215c0d2d5c5169e4d6162f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:37:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'प्रशासक अनुप्रयोग, आपण प्रणाली वापरकर्त्यांची गट विशिष्ट अॅप्स प्रवेश मंजूर करू शकता.';
+$lang['administrators_app_name'] = 'प्रशासक';
