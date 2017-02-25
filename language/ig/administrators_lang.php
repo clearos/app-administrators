f9f7525a250b8d47eea8a875f38519e1662fd3ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:35:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Na Administrators ngwa, ị nwere ike nye ohere kpọmkwem ngwa ọdịnala dị iche iche nke ndị ọrụ na usoro.';
+$lang['administrators_app_name'] = 'Administrators';
