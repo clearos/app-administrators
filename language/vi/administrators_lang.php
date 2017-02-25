b3a46b520a9685aa3c62b710359e2c6031891ec2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:42:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'Với các ứng dụng quản trị viên, bạn có thể cấp quyền truy cập vào các ứng dụng cụ thể cho các nhóm người dùng trên hệ thống.';
+$lang['administrators_app_name'] = 'Quản trị';
