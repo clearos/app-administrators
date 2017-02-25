a2c5678cf56ebc3b1ff0b918da2379bc91100bf4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 12:42:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/administrators_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['administrators_app_description'] = 'With lokusebenza Administrators, onganikeza ukufinyelela izinhlelo zokusebenza letsite amaqembu abasebenzisi ohlelweni.';
+$lang['administrators_app_name'] = 'Abalawuli';
